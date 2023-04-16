4507427d93692873954096989a7789a70ecc3a28
flying-YT <58200032+flying-YT@users.noreply.github.com>
2023-04-16 23:21:58 +0900

    Add files via upload

---
+++ 01/Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+	public static void Main()
+	{
+		System.Console.WriteLine("Hello World");
+		System.Console.WriteLine("トビーです。");
+		System.Console.WriteLine("プログラミングを始めました！");
+	}
+}
